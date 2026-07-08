a5da6ce87df33bd1c42a393337be914d0ff005b7
Ilya Yahnis <ilavoroev@gmail.com>
2026-07-08 04:33:37 +0300

    fix

--- src/main/java/org/example/clientPackage/Client.java
+++ src/main/java/org/example/clientPackage/Client.java
@@ -2,6 +2,6 @@ package org.example.clientPackage;
 
 public class Client {
     public static void main(String[] args){
-        mainMenu.main(args);
+        mainMenu.goUI(args);
     }
 }
--- src/main/java/org/example/clientPackage/mainMenu.java
+++ src/main/java/org/example/clientPackage/mainMenu.java
@@ -1,15 +1,21 @@
 package org.example.clientPackage;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class mainMenu extends Application {
-
+    public BorderPane root = new BorderPane();
+    public Scene scene = new Scene(root);
     @Override
     public void start(Stage stage) throws Exception {
+
+
+        stage.setScene(scene);
         stage.show();
     }
-    public static void main(String[] args){
+    public static void goUI(String[] args){
         launch(args);
     }
 }
