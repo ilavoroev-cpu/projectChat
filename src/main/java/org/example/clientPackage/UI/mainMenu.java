b0f843b0e77a5a7a72c1d0207d4e1acf928e3356
Ilya Yahnis <ilavoroev@gmail.com>
2026-07-08 14:34:13 +0300

    fix color

--- src/main/java/org/example/clientPackage/UI/mainMenu.java
+++ src/main/java/org/example/clientPackage/UI/mainMenu.java
@@ -5,6 +5,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.scene.control.ScrollPane;
@@ -15,17 +16,24 @@ import javafx.scene.control.ScrollPane;
 public class mainMenu extends Application {
     private BorderPane root = new BorderPane();
     private Scene scene = new Scene(root);
-    private VBox chats = new VBox(15);
+    private VBox chats = new VBox();
     private ScrollPane scrollPane = new ScrollPane(chats);
     @Override
     public void start(Stage stage) throws Exception {
         root.setCenter(scrollPane);
-        chats.setPadding(new Insets(10, 8, 10, 8));
+
         //подргрузка данных из бд
         for (int i = 0; i < 50; i++){
-
-            chats.getChildren().add(new Rectangle(300,400));
+            var r = new Rectangle(200,100);
+            r.setFill(Color.LIGHTBLUE);
+            r.setStroke(Color.BLACK);
+            chats.getChildren().add(r);
+            r.setOnMouseClicked(event -> {
+                //реалезация перехода на чат
+            });
         }
+
+
         stage.setScene(scene);
         stage.show();
     }
