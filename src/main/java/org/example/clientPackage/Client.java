fa9290463eae555f247427a3a1da8528779053ad
Ilya Yahnis <ilavoroev@gmail.com>
2026-07-08 05:04:06 +0300

    add new package with client

--- src/main/java/org/example/clientPackage/Client.java
+++ src/main/java/org/example/clientPackage/Client.java
@@ -1,5 +1,7 @@
 package org.example.clientPackage;
 
+import org.example.clientPackage.UI.mainMenu;
+
 public class Client {
     public static void main(String[] args){
         mainMenu.goUI(args);
--- src/main/java/org/example/clientPackage/UI/mainMenu.java
+++ src/main/java/org/example/clientPackage/UI/mainMenu.java
@@ -1,20 +1,31 @@
-package org.example.clientPackage;
+package org.example.clientPackage.UI;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.scene.control.ScrollPane;
 
 
+
+
 public class mainMenu extends Application {
-    private ScrollPane scrollPane = new ScrollPane();
     private BorderPane root = new BorderPane();
     private Scene scene = new Scene(root);
+    private VBox chats = new VBox(15);
+    private ScrollPane scrollPane = new ScrollPane(chats);
     @Override
     public void start(Stage stage) throws Exception {
         root.setCenter(scrollPane);
+        chats.setPadding(new Insets(10, 8, 10, 8));
+        //подргрузка данных из бд
+        for (int i = 0; i < 50; i++){
 
+            chats.getChildren().add(new Rectangle(300,400));
+        }
         stage.setScene(scene);
         stage.show();
     }
