2e05fd34bb272e3663f856e573c827c44939a917
Ilya Yahnis <ilavoroev@gmail.com>
2026-07-08 04:16:43 +0300

    add package and new class mainMenu

---
+++ src/main/java/org/example/clientPackage/Client.java
@@ -0,0 +1,7 @@
+package org.example.clientPackage;
+
+public class Client {
+    public static void main(String[] args){
+        mainMenu.main(args);
+    }
+}
---
+++ src/main/java/org/example/clientPackage/mainMenu.java
@@ -0,0 +1,15 @@
+package org.example.clientPackage;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class mainMenu extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
+}
